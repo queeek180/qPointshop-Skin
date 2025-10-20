542b3588692580ff91922881f7bddb2dac90f37e
queeek180 <qk@queeek180.com>
2025-10-20 06:44:47 +0000

    Delete items/powerups directory

--- items/powerups/__category.lua
+++
@@ -1,8 +0,0 @@
-CATEGORY.Name = 'Powerups'
-CATEGORY.Icon = 'star'
-
-CATEGORY.EnableSubcategories = true -- currently unused. will be required once support for disabling subcategories is added
-CATEGORY.DefaultSubcategoryName = "Cosmetic" -- if an item does not have a subcategory set, it will default to this
-CATEGORY.SubcategoryOrder = {
-	"Cosmetic",
-}
