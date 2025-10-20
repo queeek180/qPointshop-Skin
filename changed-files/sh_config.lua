537136dfc0a4aa2e8e8fc797050c45dd96a321aa
queeek180 <qk@queeek180.com>
2025-10-20 06:26:45 +0000

    Update sh_config.lua

--- changed-files/sh_config.lua
+++ changed-files/sh_config.lua
@@ -2,9 +2,9 @@ PS.Config = {}
 
 -- Edit below
 
-PS.Config.CommunityName = "qCorp Vending, inc."
+PS.Config.CommunityName = "Pointshop"
 
-PS.Config.DataProvider = 'mysql'
+PS.Config.DataProvider = 'pdata'
 
 PS.Config.Branch = 'https://raw.github.com/adamdburton/pointshop/master/' -- Master is most stable, used for version checking.
 PS.Config.CheckVersion = true -- Do you want to be notified when a new version of Pointshop is avaliable?
